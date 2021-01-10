943c0df95a49f58bc19fdcacd6045f133ebc2e77
RebeccaHA <rebeccahunt034@gmail.com>
2021-01-10 10:19:26 +0000

    added v-for to loop through array rather than hardcode

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      friends: []
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com"
+        },
+        {
+          id: "Julie",
+          name: "Julie Jones",
+          phone: "01234 5678 991",
+          email: "julie@localhost.com"
+        }
+      ]
     };
   }
 });
